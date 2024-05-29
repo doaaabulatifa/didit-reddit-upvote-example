71c4ce1ee2dc437324ab1f0b30fed87ffd03deab
doaaabulatifa <doaalatifa1@gmail.com>
2024-05-29 23:59:33 +0100

    added profile

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -27,6 +27,12 @@ export default async function RootLayout({ children }) {
             >
               Add post
             </Link>
+            <Link
+              href="/user-profile"
+              className="ml-10 hover:bg-zinc-300 p-2 rounded bg-pink-300 text-black"
+            >
+              User profile
+            </Link>
             <div className="ml-auto">
               <UserInfo />
             </div>
---
+++ src/app/user-profile/page.jsx
@@ -0,0 +1,55 @@
+import auth from "../../app/middleware";
+// import { LoginButton } from "./LoginButton";
+// import { LogoutButton } from "./LogoutButton";
+
+export default async function myProfile() {
+    const { userId } =  await auth();
+    console.log({userId});
+    const result = await db.query("SELECT * FROM userss WHERE id = $1", [userId]);
+ 
+  
+  const user = result.rows[0]; 
+
+  async function editProfile(formData) {
+    "use server";
+    const name = formData.get("name");
+    const email= formData.get("email");
+    const image= formData.get("image");
+  
+    await db.query(
+        "UPDATE users SET name = $1, email = $2,image= $3 WHERE id = $4",
+        [name, email, image, userId]
+      );
+    // revalidatePath("/");
+  
+    // redirect("/");
+  }
+
+  return (
+    <div>
+      <form action={editProfile}>
+        <label>Name</label>
+        <input
+          name="name"
+          placeholder="your Name"
+          defaultValue={user.name}
+        />
+        <input
+          name="email"
+          placeholder="your email"
+          defaultValue={user.email}
+         
+        />
+        <input
+          name="image"
+          placeholder="your image"
+          defaultValue={user.image}
+        />
+   
+
+       
+        <button>Submit Changes</button>
+      </form>
+    </div>
+  );
+}
